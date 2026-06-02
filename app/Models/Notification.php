267404e12bea5d0d643b441106e464fd96285b49
Felipe Pietro <nuhhgamer@gmail.com>
2026-06-01 21:09:58 -0300

    Controller e Model das notificações

---
+++ app/Models/Notification.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    protected $table = 'notificacao';
+
+    protected $primaryKey = 'sq_notificacao';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'status_confirmacao',
+        'mensagem_notificacao',
+    ];
+}
